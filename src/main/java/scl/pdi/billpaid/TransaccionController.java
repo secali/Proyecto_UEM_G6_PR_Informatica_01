794b7edef4628bf849528e2369ca34d59eb8a107
Sergio <sergio13896@gmail.com>
2023-02-02 12:03:53 +0100

    Añadida funcionalidad para exportar transacciones

--- src/main/java/scl/pdi/billpaid/TransaccionController.java
+++ src/main/java/scl/pdi/billpaid/TransaccionController.java
@@ -9,17 +9,22 @@ import javafx.scene.text.Text;
 import scl.pdi.billpaid.modelo.Transaccion;
 import scl.pdi.billpaid.modelo.User;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.sql.ClientInfoStatus;
 import java.time.Clock;
 import java.util.ArrayList;
 
-public class TransaccionController extends MainPanelController{
+public class TransaccionController extends MainPanelController {
     private Transaccion transaccion;
     private ArrayList<Transaccion> transacciones_almacenadas = new ArrayList<>();
-    private ArrayList<User>deudores = new ArrayList<>();
+    private ArrayList<User> deudores = new ArrayList<>();
     @FXML
     private Label lb_cantidad;
     @FXML
-    private ListView <String> list_transacciones;
+    private ListView<String> list_transacciones;
     @FXML
     private TextField tf_nombre_transaccion;
     @FXML
@@ -38,7 +43,7 @@ public class TransaccionController extends MainPanelController{
     @FXML
     protected void onCrearTransaccionButtonClick() {
 
-        ArrayList<String>pagadores = new ArrayList<>();
+        ArrayList<String> pagadores = new ArrayList<>();
         pagadores.add(tf_pagador_por.getText()); //HABRIA QUE SEPARAR POR COMAS Y METERLOS EN EL ARRAYLIST
 
         //deudoSres.add(tf_deber_por.getText());
@@ -47,15 +52,14 @@ public class TransaccionController extends MainPanelController{
 
         //for (String subString : deudores_str) {
 
-          //  deudores.add(new User(subString, "", "usuario"));
+        //  deudores.add(new User(subString, "", "usuario"));
         //}
 
-         //HABRIA QUE SEPARAR POR COMAS Y METERLOS EN EL ARRAYLIST
+        //HABRIA QUE SEPARAR POR COMAS Y METERLOS EN EL ARRAYLIST
 
 
-
-        transaccion = new Transaccion("1",tf_nombre_transaccion.getText(), tf_descrip_trans.getText(), Double.parseDouble(tf_cantidad.getText()),
-                "normal",pagadores,deudores, date_fecha_transaccion.getValue().toString());
+        transaccion = new Transaccion("1", tf_nombre_transaccion.getText(), tf_descrip_trans.getText(), Double.parseDouble(tf_cantidad.getText()),
+                "normal", pagadores, deudores, date_fecha_transaccion.getValue().toString());
 
 
         //Usa el método de Transaccion.java para pasar a String los parametros relevantes y devolverlos en una cadena que pinta el listview
@@ -66,10 +70,7 @@ public class TransaccionController extends MainPanelController{
 
         //suma la cantidad de cada transaccion introducida y actualiza el indicador
         cantidad += transaccion.getCantidad();
-        lb_cantidad.setText(Double.toString(cantidad)+" €");
-
-
-
+        lb_cantidad.setText(Double.toString(cantidad) + " €");
 
 
     }
@@ -80,17 +81,17 @@ public class TransaccionController extends MainPanelController{
     protected void onEliminarTransaccionSUPR(KeyEvent event) {
 
 
-            if (event.getCode() == KeyCode.DELETE) {
-                int idx_eliminar = list_transacciones.getSelectionModel().getSelectedIndex();
-                if (idx_eliminar >= 0) {
-                    list_transacciones.getItems().remove(idx_eliminar);  //elimina de la lista
+        if (event.getCode() == KeyCode.DELETE) {
+            int idx_eliminar = list_transacciones.getSelectionModel().getSelectedIndex();
+            if (idx_eliminar >= 0) {
+                list_transacciones.getItems().remove(idx_eliminar);  //elimina de la lista
 
-                    //Actualiza la cantidad mostrada y elimina el objeto almacenado
-                    cantidad -= transacciones_almacenadas.get(idx_eliminar).getCantidad();
-                    lb_cantidad.setText(Double.toString(cantidad) + " €");
-                    transacciones_almacenadas.remove(idx_eliminar);
-                }
+                //Actualiza la cantidad mostrada y elimina el objeto almacenado
+                cantidad -= transacciones_almacenadas.get(idx_eliminar).getCantidad();
+                lb_cantidad.setText(Double.toString(cantidad) + " €");
+                transacciones_almacenadas.remove(idx_eliminar);
             }
+        }
 
     }
 
@@ -110,6 +111,22 @@ public class TransaccionController extends MainPanelController{
             transacciones_almacenadas.remove(idx_eliminar);
         }
     }
+
+    @FXML
+    protected void onExportarTransaccionesClick() {
+        try {
+            FileWriter writer = new FileWriter("transacciones_exportadas.txt");
+
+            writer.write(list_transacciones.getItems().toString());
+            writer.write(System.lineSeparator());
+            writer.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+}
 
 
